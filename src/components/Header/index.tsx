a6ed14d6d70cd7d4f07955288a1c2d581f93f52a
Victor Ribeiro <hyukodev@gmail.com>
2022-01-22 20:11:34 -0300

    feat(components): add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image
+          src='/images/logo.svg'
+          width={110}
+          height={110}
+          alt="ig.news Logo" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}

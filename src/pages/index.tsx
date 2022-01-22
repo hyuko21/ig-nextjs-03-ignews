9fc1d432c2fade664b54c6a453e2c93800d31e99
Victor Ribeiro <hyukodev@gmail.com>
2022-01-22 19:46:22 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        <span>Hello</span> World
+      <h1>
+        Hello World
       </h1>
     </>
   )

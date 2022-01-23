0a304b810198bf67afc0521ed98d314d9afe7b08
Victor Ribeiro <hyukodev@gmail.com>
2022-01-22 21:34:21 -0300

    feat: add stripe and connect with stripe api (on server-side)

---
+++ src/services/stripe.ts
@@ -0,0 +1,13 @@
+import Stripe from 'stripe'
+import pkg from '../../package.json'
+
+export const stripe = new Stripe(
+  process.env.STRIPE_API_KEY,
+  {
+    apiVersion: '2020-08-27',
+    appInfo: {
+      name: 'hyuko.dev',
+      version: pkg.version
+    }
+  }
+)
